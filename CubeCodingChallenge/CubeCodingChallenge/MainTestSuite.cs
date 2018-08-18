7ce9f391581ef8c4a2042a75d5beb2456a2fb819
Shinu <mathewshinu9474@gmail.com>
2018-08-18 06:27:01 +0530

    first commit

---
+++ CubeCodingChallenge/CubeCodingChallenge/MainTestSuite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CubeCodingChallenge.TestCases;
+using CubeCodingChallenge.Variables;
+
+namespace CubeCodingChallenge
+{
+    class MainTestSuite
+    {
+        static void Main(string[] args)
+        {
+            //TestCase.GenerateToken();
+
+            UtilityMethods.GenerateToken();
+            TestCase.GetListOfFiles();
+            TestCase.UploadFile();
+            TestCase.VerifyUploadedFile();
+            TestCase.DeleteSpecificFile();
+            TestCase.ShareFile();
+            TestCase.VerifySharedFile();
+        }
+    }
+}
